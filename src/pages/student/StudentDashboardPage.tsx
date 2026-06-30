337074a657cc19d551d3d4643e28d3e6b740cf6f
MateoOjeda <mateo_ojeda_lado@hotmail.com>
2026-06-30 10:50:22 -0300

    fix: resolver colision de nombre de Dumbbell en Dashboard

--- src/pages/student/StudentDashboardPage.tsx
+++ src/pages/student/StudentDashboardPage.tsx
@@ -524,23 +524,3 @@ export default function StudentDashboardPage() {
     </div>
   );
 }
-
-const Dumbbell = (props: any) => (
-  <svg
-    {...props}
-    xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-  >
-    <path d="M6.5 6.5h11" />
-    <path d="M6.5 17.5h11" />
-    <path d="m3 21 18-18" />
-    <path d="m3 3 18 18" />
-  </svg>
-);
